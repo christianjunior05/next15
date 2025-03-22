be67af555c02c6c922987428dc456bde7dabace4
christianjunior05 <bchrist.junior@gmail.com>
2025-03-22 10:09:32 +0000

    ajout des routes conditionnés

---
+++ src/app/dashboard/@loginuser/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/app/components/Card";
+
+export default function LoginUser(){
+    return(
+        <Card>
+            <p>Veuillez vous connectez pour continuer svp </p>
+        </Card>
+    )
+}
