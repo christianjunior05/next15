3200216a693cab9d3980868d53cb11a931008b1c
christianjunior05 <bchrist.junior@gmail.com>
2025-03-22 10:24:22 +0000

    add

--- src/app/fold/page.tsx
+++ src/app/fold/page.tsx
@@ -2,6 +2,9 @@ import Link  from "next/link";
 
 export default function Fold(){
     return (
+        <div className="w-full h-screen flex items-center justify-center">
 
+            <h1 className="text-center text-slate text-4xl my-6"></h1>
+        </div>
     )
 }
