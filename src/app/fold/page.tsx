71400d0d8c9462c1a2d15bae36efeb334148143d
christianjunior05 <bchrist.junior@gmail.com>
2025-03-22 10:25:21 +0000

    add

--- src/app/fold/page.tsx
+++ src/app/fold/page.tsx
@@ -4,7 +4,8 @@ export default function Fold(){
     return (
         <div className="w-full h-screen flex items-center justify-center">
 
-            <h1 className="text-center text-slate text-4xl my-6"></h1>
+            <h1 className="text-center text-slate text-4xl my-6">Page fold</h1>
+            <Link href="/fold/fold2">Fold 2</Link>
         </div>
     )
 }
