dd5feff099aec842c38df00d1cc4c8d6d92118d7
christianjunior05 <bchrist.junior@gmail.com>
2025-03-22 10:26:06 +0000

    add

---
+++ src/app/fold/fold2/page.tsx
@@ -0,0 +1,11 @@
+import Link  from "next/link";
+
+export default function Fold2(){
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+
+            <h1 className="text-center text-slate text-4xl my-6">Page fold2</h1>
+            
+        </div>
+    )
+}
