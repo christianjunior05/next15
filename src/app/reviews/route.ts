07d08d9e361f22338ac80b2320ce714a51f86deb
christianjunior05 <bchrist.junior@gmail.com>
2025-03-23 11:14:56 +0000

    add

--- src/app/reviews/route.ts
+++ src/app/reviews/route.ts
@@ -3,3 +3,20 @@ import { reviewsTabs } from "./data";
 export async function GET(){
     return Response.json(reviewsTabs)
 }
+
+export async function POST(request:Request){
+    const review= await request.json()
+    const newReview ={
+        id:reviewsTabs.length+1, 
+        text: review.text()
+    }
+
+     reviewsTabs.push(newReview)
+
+     return new Response(JSON.stringify(newReview),{
+        headers :{
+            "Content-Type":"application/json"
+        },
+        status:201
+     })
+}
